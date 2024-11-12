2361a7e962031bf14599c627eba5a94d87b5966d
Prakashkakarlla <prakash.20bce7047@vitap.ac.in>
2024-11-12 10:48:37 +0530

    fi

--- server.js
+++ server.js
@@ -1,60 +1,18 @@
+// Import the express module
 const express = require('express');
-const nodemailer = require('nodemailer');
-const cors = require('cors'); // To allow CORS if frontend and backend are on different servers
-require('dotenv').config(); // Load environment variables
+
+// Create an instance of an express application
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(express.json());
+// Define the port
+const PORT = 5000;
 
-
-
-// Enable CORS for requests from your frontend
-app.use(cors({
-    origin: 'https://prakashkakarlla.github.io/Prakashkakarlla', // Allow only your frontend origin
-}));
-
-
+// Set up a simple route that sends "Hello World" as a response
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
-// Configure Nodemailer
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER, // Use environment variable for email
-        pass: process.env.EMAIL_PASS, // replace with your email password
-    },
-});
-
-//user: 'kakarllaprakash108@gmail.com', // replace with your email
-//pass: 'dgmt bfoy xied lniw', // replace with your email password
-
-
-// POST route to handle form submissions
-app.post('/send', (req, res) => {
-    const { name, email, subject, description } = req.body;
-
-    const mailOptions = {
-        from: email,
-        to: `${email}`, // replace with the recipient email
-        subject: `Subject ${subject}`,
-        text: `Name: ${name}\n\nDescription:\n${description}`,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Error sending mail:', error); // Log the error for debugging
-            return res.status(500).json({ error: 'Failed to send email', details: error.message });
-        }
-        res.status(200).json({ message: 'Email sent successfully' });
-    });
-    
-});
-
-// Start server
+// Start the server on the specified port
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
